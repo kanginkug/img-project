8d5fb237b9dc45a508d088bc1203a24bf80ba061
kik <rkddlsrnr1234@gmail.com>
2023-07-15 00:26:43 +0900

    프로젝트 생성

---
+++ src/main/java/com/side/imgproject/ImgProjectApplication.java
@@ -0,0 +1,13 @@
+package com.side.imgproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/side/imgproject/ImgProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.imgproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
